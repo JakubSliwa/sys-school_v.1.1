7f12645cea87d4bb883ce902f4fb911533f6a21d
Jakub Śliwa <jakubsliwa93@gmail.com>
2018-03-23 19:11:26 +0100

    User_Group model and dao methods added

--- src/dao/UserGroupDao.java
+++ src/dao/UserGroupDao.java
@@ -8,9 +8,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 import db.DbUtil;
+import model.Exercise;
 import model.UserGroup;
 
 public class UserGroupDao {
+
+	public static boolean saveToDB(UserGroup group) {
+		try (Connection conn = DbUtil.getConn()) {
+
+			String sql = "INSERT INTO user_group(name) VALUES (?)";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, group.getUserGroupName());
+			preparedStatement.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean updateAllUserGroupParameter(int id, String name) {
+		try (Connection conn = DbUtil.getConn()) {
+
+			String sql = "UPDATE user_group	SET	name=? where id = ?";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setString(1, name);
+			preparedStatement.setInt(2, id);
+			preparedStatement.executeUpdate();
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public void delete(int id) {
+		try (Connection conn = DbUtil.getConn()) {
+			PreparedStatement preparedStatement = conn.prepareStatement("DELETE	FROM user_group WHERE	id=	?");
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			id = 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static UserGroup loadById(int id) {
+		UserGroup group = null;
+		try (Connection conn = DbUtil.getConn()) {
+
+			String sql = "SELECT * FROM user_group WHERE id=?;";
+			PreparedStatement preparedStatement = conn.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			ResultSet rs = preparedStatement.executeQuery();
+			if (rs.next()) {
+				String userGroupName = rs.getString("name");
+				group = new UserGroup(id, userGroupName);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return group;
+	}
+
 	public static List<UserGroup> loadAll() {
 		List<UserGroup> result = new ArrayList<>();
 		try (Connection conn = DbUtil.getConn()) {
--- src/model/UserGroup.java
+++ src/model/UserGroup.java
@@ -1,33 +1,43 @@
 package model;
 
 public class UserGroup {
-	private Integer id;
-	private String name;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public UserGroup(Integer id, String name) {
-
-		this.id = id;
-		this.name = name;
-	}
+	private int id;
+	private String userGroupName;
 
 	public UserGroup() {
 
 	}
 
+	public UserGroup(int id, String name) {
+
+		this.id = id;
+		this.userGroupName = name;
+	}
+
+	public UserGroup(String userGroupName) {
+
+		this.userGroupName = userGroupName;
+	}
+
+	@Override
+	public String toString() {
+		return "Nazwa grupy to " + userGroupName + ", a jej id to: " + id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserGroupName() {
+		return userGroupName;
+	}
+
+	public void setUserGroupName(String userGroupName) {
+		this.userGroupName = userGroupName;
+	}
 }
